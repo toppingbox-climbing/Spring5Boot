605748e28ce7ad837ba48fce931eaccf81f8b9c1
cherry <just.come.as.you.are.babe@gmail.com>
2023-07-04 11:07:16 +0900

    프로젝트 초기화

---
+++ README.md
@@ -0,0 +1,6 @@
+# Spring5Boot
+*SpringMVC
+*Spring DEV Tools
+*Thymeleaf
+*mybatis
+*MariaDB
---
+++ src/test/java/cherry/hello/boot/spring5boot/Spring5BootApplicationTests.java
@@ -0,0 +1,13 @@
+package cherry.hello.boot.spring5boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5BootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
