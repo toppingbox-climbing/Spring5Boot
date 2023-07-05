e116e4b966d871c7aea28e191274631857cc67c9
cherry <just.come.as.you.are.babe@gmail.com>
2023-07-05 10:59:02 +0900

    회원가입: Member 클래스 추가

---
+++ src/main/java/cherry/hello/boot/spring5boot/model/Member.java
@@ -0,0 +1,22 @@
+package cherry.hello.boot.spring5boot.model;
+
+import lombok.*;
+
+@Setter
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Member {
+    private String mno;
+    private String userid;
+    private String passwd;
+    private String name;
+    private String email;
+    private String zipcode;
+    private String addr1;
+    private String addr2;
+    private String phone;
+    private String regdate;
+}
