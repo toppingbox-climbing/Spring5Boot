bad73732c7ecc35631a5c5aa78a5a63a25ee61d3
cherry <just.come.as.you.are.babe@gmail.com>
2023-07-06 15:51:16 +0900

    회원가입 : 실명 확인 처리

--- src/main/java/cherry/hello/boot/spring5boot/controller/JoinController.java
+++ src/main/java/cherry/hello/boot/spring5boot/controller/JoinController.java
@@ -1,10 +1,14 @@
 package cherry.hello.boot.spring5boot.controller;
 
+import cherry.hello.boot.spring5boot.model.Checkme;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/join")
@@ -32,6 +36,22 @@ public class JoinController {
 
         return "join/checkme";
     }
+
+    @PostMapping("/checkme")
+    public String checkmeok(Checkme checkme, HttpSession sess) {
+        logger.info("join/checkmeok 호출!!");
+        // checkme에 보낸 개인정보를 세션에 저장하고
+        // joinme로 이동
+        String viewPage = "redirect:/join/checkme";
+
+        if (checkme.getName() != null) {
+            sess.setAttribute("checkme", checkme);
+            viewPage = "redirect:/join/joinme";
+        }
+
+        return viewPage;
+    }
+
 
     @GetMapping("/joinok")
     public String joinok() {
