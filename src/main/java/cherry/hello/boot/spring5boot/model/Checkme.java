f4199653d4d777a668f7bef9505048d390f21d9d
cherry <just.come.as.you.are.babe@gmail.com>
2023-07-06 15:50:45 +0900

    회원가입 : 실명 확인 정보용 클래스

---
+++ src/main/java/cherry/hello/boot/spring5boot/model/Checkme.java
@@ -0,0 +1,15 @@
+package cherry.hello.boot.spring5boot.model;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+public class Checkme {
+    private String name;
+    private String jumin1;
+    private String jumin2;
+}
