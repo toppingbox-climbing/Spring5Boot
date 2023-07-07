224d253c3788a351fe640ed83cd41dbc682a7bb0
cherry <just.come.as.you.are.babe@gmail.com>
2023-07-07 16:21:09 +0900

    우편번호 조회 : MemberService findzip 구현

--- src/main/java/cherry/hello/boot/spring5boot/service/MemberService.java
+++ src/main/java/cherry/hello/boot/spring5boot/service/MemberService.java
@@ -1,10 +1,14 @@
 package cherry.hello.boot.spring5boot.service;
 
 import cherry.hello.boot.spring5boot.model.Member;
+import com.fasterxml.jackson.core.JsonProcessingException;
+
 import java.util.List;
 
 public interface MemberService {
 
     boolean saveMember(Member m);
     List<Member> readMember();
+
+    String findzip(String dong) throws JsonProcessingException;
 }
