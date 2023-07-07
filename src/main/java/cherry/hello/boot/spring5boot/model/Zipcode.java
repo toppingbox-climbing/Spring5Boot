466fd0df3c1c1fd663a5be1b96196bf812abf9ea
cherry <just.come.as.you.are.babe@gmail.com>
2023-07-07 16:22:25 +0900

    우편번호 조회 : Zipcode 클래스 추가

---
+++ src/main/java/cherry/hello/boot/spring5boot/model/Zipcode.java
@@ -0,0 +1,20 @@
+package cherry.hello.boot.spring5boot.model;
+
+
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
+public class Zipcode {
+    private String zipcode;
+    private String sido;
+    private String gugun;
+    private String dong;
+    private String ri;
+    private String bunji;
+
+}
