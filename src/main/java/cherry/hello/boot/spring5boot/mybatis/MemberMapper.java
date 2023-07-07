fdfac238778bef08a9963d19b446c288f6107cbe
cherry <just.come.as.you.are.babe@gmail.com>
2023-07-07 11:57:41 +0900

    회원가입: MemberMapper 인터페이스 추가

---
+++ src/main/java/cherry/hello/boot/spring5boot/mybatis/MemberMapper.java
@@ -0,0 +1,12 @@
+package cherry.hello.boot.spring5boot.mybatis;
+
+import cherry.hello.boot.spring5boot.model.Member;
+import org.apache.ibatis.annotations.Mapper;
+import java.util.List;
+
+@Mapper
+public interface MemberMapper {
+        int insertMember(Member m);
+        List<Member> selectMember();
+    }
+
