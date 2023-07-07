f451a7678dab6e057beacfe1cc53038a13de0351
cherry <just.come.as.you.are.babe@gmail.com>
2023-07-07 10:40:11 +0900

    회원가입: MemberDAO 단위 테스트 추가(실패)

---
+++ src/test/java/cherry/hello/boot/spring5boot/MemberDAOUnitTest.java
@@ -0,0 +1,39 @@
+package cherry.hello.boot.spring5boot;
+
+
+import cherry.hello.boot.spring5boot.dao.MemberDAO;
+import cherry.hello.boot.spring5boot.dao.MemberDAOImpl;
+import cherry.hello.boot.spring5boot.model.Member;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.runner.RunWith;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@RunWith(SpringRunner.class)
+@MybatisTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Import(MemberDAOImpl.class)
+
+public class MemberDAOUnitTest {
+
+    @Autowired
+    private MemberDAO mdao;
+
+    @Test
+    @DisplayName("MemberDAO insert Test")
+    void insertMember() {
+        Member m = new Member(null,"","","","",
+                "","","","",null);
+
+        int result = mdao.insertMember(m);
+        System.out.println(result);
+        assertEquals(result, 1);
+
+    }
+}
