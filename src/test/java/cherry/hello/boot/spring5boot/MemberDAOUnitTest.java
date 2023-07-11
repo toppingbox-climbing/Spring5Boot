9a292f1a72a0b85d419d14b33fb1594c5fc9edee
cherry <just.come.as.you.are.babe@gmail.com>
2023-07-11 11:56:41 +0900

    로그인  : MemberDAO selectOneMember 단위테스트

--- src/test/java/cherry/hello/boot/spring5boot/MemberDAOUnitTest.java
+++ src/test/java/cherry/hello/boot/spring5boot/MemberDAOUnitTest.java
@@ -33,12 +33,24 @@ public class MemberDAOUnitTest {
     }
 
     @Test
-    @DisplayName("MemberMapper checkUserid Test")
+    @DisplayName("MemberDAO checkUserid Test")
     void checkUserid() {
         String uid = "abc123";
        int result = mdao.selectOneUserid(uid);
 
         System.out.println(result);
         assertEquals(result,1);
+    }
+
+    @Test
+    @DisplayName("MemberDAO selectOneMember Test")
+    void selectOneMember() {
+        Member m = new Member();
+        m.setUserid("abc123");
+        m.setPasswd("987xyz");
+
+        Member result = mdao.selectOneMember(m);
+        System.out.println(result);
+        assertNotNull(result);
     }
 }
