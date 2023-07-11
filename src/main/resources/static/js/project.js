4bd75d51fafe4fdbc0acdbf11ecff963aab07476
cherry <just.come.as.you.are.babe@gmail.com>
2023-07-11 12:00:01 +0900

    로그인  : 로그인 폼 및 처리 구현

--- src/main/resources/static/js/project.js
+++ src/main/resources/static/js/project.js
@@ -204,3 +204,20 @@ go2idx?.addEventListener('click', () => {
     location.href="/";
 
 });
+
+// login
+let loginbtn = document.querySelector("#loginbtn");
+let lguid = document.querySelector("#userid");
+let lgpwd = document.querySelector("#passwd");
+let lgfrm = document.querySelector("#lgnfrm");
+loginbtn?.addEventListener('click', () => {
+
+    if (lguid.value ==='') alert('아이디 입력하세요!!');
+    else if (lgpwd.value ==='') alert('비밀번호 입력하세요!!');
+    else {
+        lgfrm.method = 'post';
+        lgfrm.action = '/join/login';
+        lgfrm.submit();
+    }
+
+});
