fcaf701d47836969c2530e85051411bee4a439e0
cherry <just.come.as.you.are.babe@gmail.com>
2023-07-12 15:40:52 +0900

    게시판 : boardService  본문글 구현

--- src/main/java/cherry/hello/boot/spring5boot/service/BoardServiceImpl.java
+++ src/main/java/cherry/hello/boot/spring5boot/service/BoardServiceImpl.java
@@ -26,6 +26,7 @@ public class BoardServiceImpl implements BoardService {
 
     @Override
     public Board readOneBoard(String bno) {
-        return null;
+
+        return bdao.selectOneBoard(bno);
     }
 }
