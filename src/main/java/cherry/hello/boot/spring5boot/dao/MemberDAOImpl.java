7de8b6940501ec4c8470d768cc1f0f201b014730
cherry <just.come.as.you.are.babe@gmail.com>
2023-07-12 10:19:48 +0900

    Member : 생성자 주입방식으로 변경

--- src/main/java/cherry/hello/boot/spring5boot/dao/MemberDAOImpl.java
+++ src/main/java/cherry/hello/boot/spring5boot/dao/MemberDAOImpl.java
@@ -17,7 +17,7 @@ public class MemberDAOImpl implements MemberDAO {
 
     // mybatis 를 사용하기 위해 필요한 객체 DI
     // 단, 생성자 주입 방식 사용!
-    @Autowired
+
     final MemberMapper memberMapper;
 
 //    @Autowired
