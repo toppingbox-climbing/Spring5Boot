d08f5566889855ad5d595b99e58d23a0db93e42d
cherry <just.come.as.you.are.babe@gmail.com>
2023-07-12 15:42:17 +0900

    게시판 : boardController view 구현

--- src/main/java/cherry/hello/boot/spring5boot/controller/BoardController.java
+++ src/main/java/cherry/hello/boot/spring5boot/controller/BoardController.java
@@ -7,6 +7,7 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,12 +19,21 @@ public class BoardController {
 
     Logger logger = LogManager.getLogger(BoardController.class);
 
-   @GetMapping("/list")
-    public String list(Model m, Integer cpg) {
+   @GetMapping("/list/{cpg}")
+    public String list(Model m, @PathVariable Integer cpg) {
        logger.info("board/list 호출!!");
 
        m.addAttribute("bds", bsrv.readBoard(cpg));
+       m.addAttribute("cpg", cpg);
 
        return "board/list";
    }
+    @GetMapping("/view/{bno}")
+    public String view(Model m, @PathVariable String bno) {
+        logger.info("board/view 호출!!");
+
+        m.addAttribute("bd", bsrv.readOneBoard(bno));
+
+        return "board/view";
+    }
 }
