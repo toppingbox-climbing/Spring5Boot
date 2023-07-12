6f22da620be30a03cb226257ea8ded1b28cfad3f
cherry <just.come.as.you.are.babe@gmail.com>
2023-07-12 18:06:12 +0900

    게시판 :  BoardController writeok 구현

--- src/main/java/cherry/hello/boot/spring5boot/controller/BoardController.java
+++ src/main/java/cherry/hello/boot/spring5boot/controller/BoardController.java
@@ -1,5 +1,6 @@
 package cherry.hello.boot.spring5boot.controller;
 
+import cherry.hello.boot.spring5boot.model.Board;
 import cherry.hello.boot.spring5boot.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.LogManager;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -35,5 +37,23 @@ public class BoardController {
         m.addAttribute("bd", bsrv.readOneBoard(bno));
 
         return "board/view";
+    }
+
+    @GetMapping("/write")
+    public String write() {
+        logger.info("board/write 호출!!");
+
+        return "board/write";
+    }
+
+    @PostMapping("/write")
+    public String writeok(Board b) {
+        logger.info("board/writeok 호출!!");
+        String returnPage = "redirect:/board/fail";
+
+        if (bsrv.saveBoard(b))
+            returnPage = "redirect:/board/list/1";
+
+        return returnPage;
     }
 }
