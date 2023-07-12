ca1c0d0da1e3a5b5d68bbfd9736cf7460ba6449c
cherry <just.come.as.you.are.babe@gmail.com>
2023-07-12 11:42:49 +0900

    게시판 : 페이징 처리를 위해 코드 수정

--- src/main/java/cherry/hello/boot/spring5boot/mybatis/BoardMapper.java
+++ src/main/java/cherry/hello/boot/spring5boot/mybatis/BoardMapper.java
@@ -1,8 +1,6 @@
 package cherry.hello.boot.spring5boot.mybatis;
 
 import cherry.hello.boot.spring5boot.model.Board;
-import cherry.hello.boot.spring5boot.model.Member;
-import cherry.hello.boot.spring5boot.model.Zipcode;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -10,7 +8,7 @@ import java.util.List;
 @Mapper
 public interface BoardMapper {
         int insertBoard(Board b);
-List<Board> selectBoard();
+List<Board> selectBoard(int stnum);
 Board selectOneBoard(String bno);
 }
 
