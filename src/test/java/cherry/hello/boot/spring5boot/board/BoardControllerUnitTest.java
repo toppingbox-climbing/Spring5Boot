6b87300e2beebc037a34f496889f11d80fd6b11b
cherry <just.come.as.you.are.babe@gmail.com>
2023-07-12 15:42:27 +0900

    게시판 : boardController view 단위테스트

--- src/test/java/cherry/hello/boot/spring5boot/board/BoardControllerUnitTest.java
+++ src/test/java/cherry/hello/boot/spring5boot/board/BoardControllerUnitTest.java
@@ -32,4 +32,12 @@ public class BoardControllerUnitTest {
                         .andExpect(status().isOk())
                         .andDo(print());
     }
+    @Test
+    @DisplayName("BoardController view Test")
+    void view() throws Exception {
+
+        mvc.perform(get("/board/view/810"))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
 }
