8cc2d215dc02d8c26d84d1f90b02a9bb7e055d40
cherry <just.come.as.you.are.babe@gmail.com>
2023-07-12 10:15:43 +0900

    게시판 :  boardDAO 단위테스트

---
+++ src/test/java/cherry/hello/boot/spring5boot/board/BoardDAOUnitTest.java
@@ -0,0 +1,30 @@
+package cherry.hello.boot.spring5boot.board;
+
+import cherry.hello.boot.spring5boot.dao.BoardDAO;
+import cherry.hello.boot.spring5boot.dao.BoardDAOImpl;
+import cherry.hello.boot.spring5boot.model.Board;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.context.annotation.Import;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@MybatisTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Import(BoardDAOImpl.class)
+
+public class BoardDAOUnitTest {
+
+    @Autowired private BoardDAO bdao;
+
+    @Test
+    @DisplayName("BoardDAO insert Test")
+    void selectBoard() {
+        List<Board> results = bdao.selectBoard();
+        assertNotNull(results);
+    }
+
+}
