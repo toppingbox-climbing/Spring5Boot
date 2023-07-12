207cbd49666922c6b6f953183a601e4c2e9c3f51
cherry <just.come.as.you.are.babe@gmail.com>
2023-07-12 18:04:12 +0900

    게시판 :  write 단위테스트

--- src/test/java/cherry/hello/boot/spring5boot/board/BoardMapperUnitTest.java
+++ src/test/java/cherry/hello/boot/spring5boot/board/BoardMapperUnitTest.java
@@ -7,9 +7,11 @@ import org.junit.jupiter.api.Test;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @MybatisTest
@@ -39,5 +41,18 @@ public class BoardMapperUnitTest {
         Board result = boardMapper.selectOneBoard(bno);
 
         assertNotNull(result);
+    }
+    @Test
+    @DisplayName("boardMapper insert Test")
+    @Transactional
+    void insertBoard() {
+        Board b = new Board();
+        b.setUserid("abc123");
+        b.setTitle("테스트");
+        b.setContents("테스트");
+        b.setIpaddr("127.0.0.1");
+
+        int result = boardMapper.insertBoard(b);
+        assertEquals(result,1);
     }
 }
