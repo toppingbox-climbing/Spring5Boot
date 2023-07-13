20bcc8e81af3ef8ecbd320e5b500dc3a69d84a13
cherry <just.come.as.you.are.babe@gmail.com>
2023-07-13 12:13:00 +0900

    게시판: BoardService 총 페이지수 조회 구현

--- src/main/java/cherry/hello/boot/spring5boot/service/BoardService.java
+++ src/main/java/cherry/hello/boot/spring5boot/service/BoardService.java
@@ -12,4 +12,6 @@ public interface BoardService {
 
     Board readOneBoard(String bno);
 
+    int countBoard();
+
 }
--- src/main/java/cherry/hello/boot/spring5boot/service/BoardServiceImpl.java
+++ src/main/java/cherry/hello/boot/spring5boot/service/BoardServiceImpl.java
@@ -31,4 +31,9 @@ public class BoardServiceImpl implements BoardService {
 
         return bdao.selectOneBoard(bno);
     }
+
+    @Override
+    public int countBoard() {
+        return bdao.selectCountBoard();
+    }
 }
