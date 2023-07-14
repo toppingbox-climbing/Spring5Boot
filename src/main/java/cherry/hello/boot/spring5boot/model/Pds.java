b64311f7a9fc13a5a6943bd1ffb26f459d3c3cb1
cherry <just.come.as.you.are.babe@gmail.com>
2023-07-14 16:04:59 +0900

    자료실 - Pds 클래스 추가

---
+++ src/main/java/cherry/hello/boot/spring5boot/model/Pds.java
@@ -0,0 +1,23 @@
+package cherry.hello.boot.spring5boot.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
+public class Pds {
+
+    private String pno;
+    private String title;
+    private String userid;
+    private String regdate;
+    private String thumbs;
+    private String views;
+    private String contents;
+    private String ipaddr;
+
+
+}
