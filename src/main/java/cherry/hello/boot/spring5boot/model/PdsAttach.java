ccc2514e029f5e4166cd53c207ff3af415c76276
cherry <just.come.as.you.are.babe@gmail.com>
2023-07-14 16:05:44 +0900

    자료실 - PdsAttach  클래스 추가

---
+++ src/main/java/cherry/hello/boot/spring5boot/model/PdsAttach.java
@@ -0,0 +1,20 @@
+package cherry.hello.boot.spring5boot.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class PdsAttach {
+
+        private String Pano;
+        private String pno;
+        private String fname;
+        private String ftype;
+        private String fsize;
+        private String fdown;
+
+    }
+
