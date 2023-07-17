ddae75f8efed775bc1944fa33473511b97a282ef
cherry <just.come.as.you.are.babe@gmail.com>
2023-07-17 13:40:20 +0900

    자료실 - PdsService : readPds, readOnePds 구현

--- src/main/java/cherry/hello/boot/spring5boot/service/PdsService.java
+++ src/main/java/cherry/hello/boot/spring5boot/service/PdsService.java
@@ -3,6 +3,7 @@ package cherry.hello.boot.spring5boot.service;
 import cherry.hello.boot.spring5boot.model.Pds;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
 import java.util.Map;
 
 public interface PdsService {
@@ -10,4 +11,10 @@ public interface PdsService {
     int newPds (Pds p);
 
     boolean newPdsAttach(MultipartFile attach, int pno);
+
+    List<Pds> readPsd(Integer cpg);
+
+    int countPds();
+
+    Pds readOnePds(String pno);
 }
--- src/main/java/cherry/hello/boot/spring5boot/service/PdsServiceImpl.java
+++ src/main/java/cherry/hello/boot/spring5boot/service/PdsServiceImpl.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
 import java.util.Map;
 
 @Service ("psrv")
@@ -35,5 +36,24 @@ final PdsUtils pdsUtils;
         int pacnt = pdao.insertPdsAttach(pa);
 
         return (pacnt > 0) ? true : false;
+    }
+
+    @Override
+    public List<Pds> readPsd(Integer cpg) {
+
+        int stnum = (cpg - 1) * 25;
+        return pdao.selectPds(stnum);
+    }
+
+    @Override
+    public int countPds() {
+        return pdao.selectCountPds();
+
+    }
+
+    @Override
+    public Pds readOnePds(String pno) {
+
+        return pdao.selectOnePds(pno);
     }
 }
